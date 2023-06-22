a206fe2586d3025050d8711d71f131c61b308b67
luis <luisguibarreto8@gmail.com>
2023-06-22 14:08:37 -0300

    Adding some css to make the site responsive and JavaScript to chnage the navbar

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
- let sections = document.querySelectorAll('section');
- let navLinks = document.querySelectorAll('header nav a');
+ /*MENU ICON NAVBAR*/
+const menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
- window.onscroll = () => {
+function menuFunction() {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+ }
+
+
+ 
+ /*SCROLL SECTION*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop -150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id')
 
-        if(top >= offset && top < of){
+        if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
             })
         };
     })
 
 
- let header = document.querySelector('.header');
+/*STYCKY NAVBAR*/
+let header = document.querySelector('.header');
 
- header.classList.toggle('sticky', window.scrollY > 100)
- }
+header.classList.toggle('sticky', window.scrollY > 100);
+
+/*REMOVE MENU ICON NAVBAR WHEN NAVBAR LINK CLICKED*/
+menuIcon.classList.remove('bx-x')
+navbar.classList.remove('active');
+
+ };
+
+ 
 
