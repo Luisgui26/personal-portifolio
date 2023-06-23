278554ad60d9b4f0140c58bcbde15f80d0ba98ed
luis <luisguibarreto8@gmail.com>
2023-06-22 21:26:29 -0300

    Making the application more responsive and adding darkmode

--- script.js
+++ script.js
@@ -41,4 +41,10 @@ navbar.classList.remove('active');
  };
 
  
+/*DARKMODE*/
+let darkModeIcon = document.querySelector('#darkMode-icon')
 
+darkModeIcon.onclick = () =>{
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
